b9b2ea52cf622bcdad7c1448b12b8778c0959315
Tiago Prata <tiago.prata@br.abb.com>
2021-11-17 18:59:00 -0300

    Parte2 do Trab DONE

--- Trabalho/parte2.py
+++ Trabalho/parte2.py
@@ -1,45 +1,55 @@
-from functools import reduce
-import operator
+# https://www.beecrowd.com.br/judge/pt/problems/view/1122
 
+from collections import defaultdict
 
-def test(A, vf, set1):
-    values = [a*b for a,b in zip(A,set1)]
-    sum = reduce(operator.add, values)
-    
-    return (sum == vf)
+class URI():
+    def __init__(self):
+        self.positivo = [False for x in range(40)]
+        self.negativo = [False for x in range(40)]
+        self.T = []
+        self.N = 0
+        self.F = 0
+        self.memo = defaultdict(dict)
 
+    def resolvedor(self, id, soma):
+        ent, saida = False, False
 
-def get_diff_results(results):
-    final = ''
-    A = results[0]
-    B = results[1]
-    for i in range(0, len(A)):
-        if A[i] != B[i]:
-            final += '?'
-        elif A[i] == -1:
-            final += '-'
-        else:
-            final += '+'
+        if (id >= self.N and soma == self.F): return True
+        elif (id >= self.N): return False
+        if (soma in self.memo) & (id in self.memo[soma]): return self.memo[soma][id]
 
-    return final
+        ent = self.resolvedor(id+1, soma + self.T[id])
+        saida = self.resolvedor(id+1, soma - self.T[id])
 
+        if(ent and not saida): self.positivo[id] = True
+        elif (not ent and saida): self.negativo[id] = True
+        elif (ent and saida):
+            self.positivo[id] = True
+            self.negativo[id] = True
+        self.memo[soma][id] = ent or saida
 
-def list_exponential(n,set1,A,vf,results=[]):
-    if n == 0:
-        print(set1)
-        if test(A, vf, set1):
-            results.append(set1)
-    else:
-        n-=1
-        list_exponential(n, [1]+set1, A, vf)
-        list_exponential(n, [-1]+set1, A, vf)
-        
-    # TODO Corrigir
-    if (len(results) > 1) and (n == 0):
-        print(get_diff_results(results))
+        return ent or saida
 
 
 if __name__=='__main__':
-    A = [1,2,3,4,5]
-    vf = 7
-    list_exponential(5,[],A,vf)
+    while (True):
+        uri = URI()
+        uri.N, uri.F = list(map(int, input().split()))
+
+        if uri.N == 0: break
+        
+        uri.positivo = [False for x in range(40)]
+        uri.negativo = [False for x in range(40)]
+        uri.T = []
+        for _ in range(0,uri.N):
+            uri.T = uri.T + [int(input())]
+
+        valido = uri.resolvedor(0,0)
+        if valido:
+            for i in range(uri.N):
+                if (uri.positivo[i] and uri.negativo[i]): print('?', end='')
+                elif (uri.positivo[i]): print('+', end='')
+                else: print('-', end='')
+        else:
+            print('*', end='')
+        print('')
